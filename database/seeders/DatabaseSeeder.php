54be795048ee5acec652500d9c05d26919062fd5
Ta1isman <146196101+tqlismqn@users.noreply.github.com>
2025-10-25 12:17:18 +0200

    fix(deploy): reset database during deploy (#17)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,19 +2,25 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Admin User',
+        // Create default admin user
+        \App\Models\User::factory()->create([
+            'name' => 'G-Track Admin',
             'email' => 'admin@g-track.eu',
             'password' => bcrypt('password123'),
         ]);
 
-        $this->call(DriversTableSeeder::class);
+        // Seed drivers with documents
+        $this->call([
+            DriversTableSeeder::class,
+        ]);
     }
 }
