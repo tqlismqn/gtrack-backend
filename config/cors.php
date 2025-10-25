8116eca07a0d3cab2edcb181068d063dd9b97808
Ta1isman <146196101+tqlismqn@users.noreply.github.com>
2025-10-25 17:18:41 +0200

    feat(api): configure cors for frontend access (#19)

---
+++ config/cors.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://app.g-track.eu',
+        'https://*.vercel.app',
+        'http://localhost:4200',
+        'http://localhost:4000',
+    ],
+
+    'allowed_origins_patterns' => [
+        '/^https:\/\/gtrack-app-.*\.vercel\.app$/',
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
